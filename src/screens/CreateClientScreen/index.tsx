95d451183c9dd06e8d807b0120dc2056242de098
unknown <mike__221@hotmail.com>
2020-11-27 21:19:12 -0600

    Create Client and Create Product Screen added but without test

--- src/screens/CreateClientScreen/index.tsx
+++ src/screens/CreateClientScreen/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import moment from "moment";
 import Container from "@material-ui/core/Container";
@@ -8,6 +9,12 @@ import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import MenuItem from "@material-ui/core/MenuItem";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { ValueLabelType } from "../../store/documentSlice/types";
 import { IApplicationState } from "../../store/rootReducer";
 import { useStyles } from "../../App.css";
@@ -39,6 +46,8 @@ type FormType = {
 
 const CreateClientScreen: React.FC<{}> = (): React.ReactElement => {
   const classes = useStyles();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [open, setOpen] = useState(false);
   const [form, setForm] = useState<FormType>({
     codeClient: "",
     businessName: "",
@@ -48,6 +57,10 @@ const CreateClientScreen: React.FC<{}> = (): React.ReactElement => {
     currency: "",
     typeClient: "",
   });
+
+  const handleClose = (): void => {
+    setOpen(false);
+  };
 
   const currencies: ValueLabelType[] = useSelector(
     (state: IApplicationState) => state.document.extra.currencies
@@ -59,6 +72,48 @@ const CreateClientScreen: React.FC<{}> = (): React.ReactElement => {
     setForm({ ...form, [name]: value });
   };
 
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5007/api/Cliente/",
+        {
+          cCodigoCliente: form.codeClient,
+          cRazonSocial: form.businessName,
+          cFechaAlta: moment(form.dateNow).format("MM/DD/YYYY"),
+          cRFC: form.rfc,
+          cCURP: form.curp,
+          cNombreMoneda: form.currency,
+          cTipoCliente: form.typeClient,
+          cEstatus: 1,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        setLoading(false);
+        setOpen(true);
+        setForm({
+          codeClient: "",
+          businessName: "",
+          dateNow: moment(Date.now()).format("YYYY-MM-DD"),
+          rfc: "",
+          curp: "",
+          currency: "",
+          typeClient: "",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Container maxWidth="md">
       <Paper className={classes.paper}>
@@ -68,120 +123,145 @@ const CreateClientScreen: React.FC<{}> = (): React.ReactElement => {
         <Typography variant="h6" gutterBottom>
           Llenar todos los campos requeridos
         </Typography>
-        <form>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                id="dateNow"
-                name="dateNow"
-                label="Fecha"
-                type="date"
-                fullWidth
-                disabled
-                defaultValue={form.dateNow}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                id="codeClient"
-                name="codeClient"
-                label="Código cliente"
-                required
-                fullWidth
-                value={form.codeClient}
-                onChange={handleInputs}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                id="businessName"
-                name="businessName"
-                label="Razón social"
-                required
-                fullWidth
-                value={form.businessName}
-                onChange={handleInputs}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="rfc"
-                name="rfc"
-                label="RFC"
-                fullWidth
-                value={form.rfc}
-                onChange={handleInputs}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                id="curp"
-                name="curp"
-                label="CURP"
-                fullWidth
-                value={form.curp}
-                onChange={handleInputs}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                select
-                id="currency"
-                name="currency"
-                label="Moneda"
-                fullWidth
-                helperText="Por favor selecciona un elemento"
-                value={form.currency}
-                onChange={handleInputs}
-              >
-                {currencies.map(
-                  (option: ValueLabelType): JSX.Element => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  )
-                )}
-              </TextField>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                select
-                id="typeClient"
-                name="typeClient"
-                label="Tipo cliente"
-                fullWidth
-                helperText="Por favor selecciona un elemento"
-                value={form.typeClient}
-                onChange={handleInputs}
-              >
-                {typeClient.map(
-                  (option: ValueLabelType): JSX.Element => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  )
-                )}
-              </TextField>
-            </Grid>
+        {loading ? (
+          <Grid container justify="center">
+            <CircularProgress size={50} />
           </Grid>
-          <div className={classes.buttons}>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              type="submit"
-            >
-              Crear
-            </Button>
-          </div>
-        </form>
+        ) : (
+          <form onSubmit={onSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="dateNow"
+                  name="dateNow"
+                  label="Fecha"
+                  type="date"
+                  fullWidth
+                  disabled
+                  defaultValue={form.dateNow}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="codeClient"
+                  name="codeClient"
+                  label="Código cliente"
+                  required
+                  fullWidth
+                  value={form.codeClient}
+                  onChange={handleInputs}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  id="businessName"
+                  name="businessName"
+                  label="Razón social"
+                  required
+                  fullWidth
+                  value={form.businessName}
+                  onChange={handleInputs}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="rfc"
+                  name="rfc"
+                  label="RFC"
+                  fullWidth
+                  value={form.rfc}
+                  onChange={handleInputs}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="curp"
+                  name="curp"
+                  label="CURP"
+                  fullWidth
+                  value={form.curp}
+                  onChange={handleInputs}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  select
+                  id="currency"
+                  name="currency"
+                  label="Moneda"
+                  fullWidth
+                  helperText="Por favor selecciona un elemento"
+                  value={form.currency}
+                  onChange={handleInputs}
+                >
+                  {currencies.map(
+                    (option: ValueLabelType): JSX.Element => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    )
+                  )}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  select
+                  id="typeClient"
+                  name="typeClient"
+                  label="Tipo cliente"
+                  fullWidth
+                  helperText="Por favor selecciona un elemento"
+                  value={form.typeClient}
+                  onChange={handleInputs}
+                >
+                  {typeClient.map(
+                    (option: ValueLabelType): JSX.Element => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    )
+                  )}
+                </TextField>
+              </Grid>
+            </Grid>
+            <div className={classes.buttons}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                type="submit"
+              >
+                Crear
+              </Button>
+            </div>
+          </form>
+        )}
       </Paper>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Alerta</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Se agregó correctamente el cliente al catálogo de clientes y
+            proveedores.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
--- src/screens/CreateProductScreen/index.tsx
+++ src/screens/CreateProductScreen/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import moment from "moment";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
@@ -14,11 +15,18 @@ import MenuItem from "@material-ui/core/MenuItem";
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Tooltip from "@material-ui/core/Tooltip";
 import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { ValueLabelType } from "../../store/documentSlice/types";
 import { useStyles } from "../../App.css";
+import { financial } from "../../utils";
 
 const typeProduct: ValueLabelType[] = [
   {
@@ -65,11 +73,68 @@ const CreateProductScreen: React.FC<{}> = (): React.ReactElement => {
     dateNow: moment(Date.now()).format("YYYY-MM-DD"),
     prices: [],
   });
+  const [loading, setLoading] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleClose = (): void => {
+    setOpen(false);
+  };
 
   const handleInputs = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
 
     setForm({ ...form, [name]: value });
+  };
+
+  interface IDataSend {
+    [key: string]: string | number[] | number;
+  }
+
+  const onSubmit = async () => {
+    setLoading(true);
+
+    const data: IDataSend = {
+      cCodigoProducto: form.codeProduct,
+      cNombreProducto: form.nameProduct,
+      cDescripcionProducto: form.description,
+      cTipoProducto: form.typeProduct,
+      cFechaAltaProducto: form.dateNow,
+      cStatusProducto: 1,
+      cClaveSAT: form.satKey,
+    };
+
+    form.prices.forEach((o: number, index: number) => {
+      let key: string = `cPrecio${index + 1}`;
+      data[key] = financial(o);
+    });
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5007/api/Producto/",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        setLoading(false);
+        setOpen(true);
+        setForm({
+          codeProduct: "",
+          nameProduct: "",
+          description: "",
+          typeProduct: "",
+          satKey: "",
+          dateNow: moment(Date.now()).format("YYYY-MM-DD"),
+          prices: [],
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -81,167 +146,195 @@ const CreateProductScreen: React.FC<{}> = (): React.ReactElement => {
         <Typography variant="h6" gutterBottom>
           Llenar todos los campos requeridos
         </Typography>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="dateNow"
-              name="dateNow"
-              label="Fecha"
-              type="date"
-              fullWidth
-              disabled
-              defaultValue={form.dateNow}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
+        {loading ? (
+          <Grid container justify="center">
+            <CircularProgress size={50} />
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="codeProduct"
-              name="codeProduct"
-              label="Código producto"
-              required
-              fullWidth
-              value={form.codeProduct}
-              onChange={handleInputs}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="nameProduct"
-              name="nameProduct"
-              label="Nombre producto"
-              required
-              fullWidth
-              value={form.nameProduct}
-              onChange={handleInputs}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              select
-              id="typeProduct"
-              name="typeProduct"
-              label="Tipo producto"
-              fullWidth
-              helperText="Por favor selecciona un elemento"
-              value={form.typeProduct}
-              onChange={handleInputs}
-            >
-              {typeProduct.map(
-                (option: ValueLabelType): JSX.Element => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                )
-              )}
-            </TextField>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="description"
-              name="description"
-              label="Descripción"
-              multiline
-              fullWidth
-              value={form.description}
-              onChange={handleInputs}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="satKey"
-              name="satKey"
-              label="Clave SAT"
-              required
-              fullWidth
-              value={form.satKey}
-              onChange={handleInputs}
-            />
-          </Grid>
-        </Grid>
-        <Box marginTop={4}>
-          <Grid
-            container
-            spacing={3}
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item xs={12} sm={4}>
-              <form
-                onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
-                  event.preventDefault();
-                  setForm({ ...form, prices: [...form.prices, price] });
-                  setPrice(0);
-                }}
-              >
+        ) : (
+          <React.Fragment>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6}>
                 <TextField
-                  id="price"
-                  name="price"
-                  label="Precio"
-                  type="number"
-                  inputProps={{ min: "0", step: "0.01" }}
-                  value={price}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                    setPrice(parseFloat(event.target.value))
-                  }
+                  id="dateNow"
+                  name="dateNow"
+                  label="Fecha"
+                  type="date"
                   fullWidth
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <AddPrice />
-                      </InputAdornment>
-                    ),
+                  disabled
+                  defaultValue={form.dateNow}
+                  InputLabelProps={{
+                    shrink: true,
                   }}
-                  helperText="Por favor agregar un elemento si desea que el producto tenga precios por defecto"
                 />
-              </form>
-              <List>
-                {form.prices.map(
-                  (value: number): JSX.Element => {
-                    const labelId = `checkbox-list-label-${value}`;
-                    return (
-                      <ListItem key={value} role={undefined} dense button>
-                        <ListItemText
-                          id={labelId}
-                          primary={`Precio: ${value}`}
-                        />
-                        <ListItemSecondaryAction>
-                          <IconButton
-                            edge="end"
-                            aria-label="comments"
-                            onClick={() => {
-                              const newPrices = form.prices.filter(
-                                (elem) => elem !== value
-                              );
-
-                              setForm({ ...form, prices: newPrices });
-                            }}
-                          >
-                            <DeleteIcon />
-                          </IconButton>
-                        </ListItemSecondaryAction>
-                      </ListItem>
-                    );
-                  }
-                )}
-              </List>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="codeProduct"
+                  name="codeProduct"
+                  label="Código producto"
+                  required
+                  fullWidth
+                  value={form.codeProduct}
+                  onChange={handleInputs}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="nameProduct"
+                  name="nameProduct"
+                  label="Nombre producto"
+                  required
+                  fullWidth
+                  value={form.nameProduct}
+                  onChange={handleInputs}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  select
+                  id="typeProduct"
+                  name="typeProduct"
+                  label="Tipo producto"
+                  fullWidth
+                  helperText="Por favor selecciona un elemento"
+                  value={form.typeProduct}
+                  onChange={handleInputs}
+                >
+                  {typeProduct.map(
+                    (option: ValueLabelType): JSX.Element => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    )
+                  )}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="description"
+                  name="description"
+                  label="Descripción"
+                  multiline
+                  fullWidth
+                  value={form.description}
+                  onChange={handleInputs}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="satKey"
+                  name="satKey"
+                  label="Clave SAT"
+                  required
+                  fullWidth
+                  value={form.satKey}
+                  onChange={handleInputs}
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-        <div className={classes.buttons}>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          >
-            Crear
-          </Button>
-        </div>
+            <Box marginTop={4}>
+              <Grid
+                container
+                spacing={3}
+                direction="row"
+                justify="center"
+                alignItems="center"
+              >
+                <Grid item xs={12} sm={4}>
+                  <form
+                    onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+                      event.preventDefault();
+                      setForm({ ...form, prices: [...form.prices, price] });
+                      setPrice(0);
+                    }}
+                  >
+                    <TextField
+                      id="price"
+                      name="price"
+                      label="Precio"
+                      type="number"
+                      inputProps={{ min: "0", step: "0.01" }}
+                      value={price}
+                      onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setPrice(parseFloat(event.target.value))
+                      }
+                      fullWidth
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <AddPrice />
+                          </InputAdornment>
+                        ),
+                      }}
+                      helperText="Por favor agregar un elemento si desea que el producto tenga precios por defecto"
+                    />
+                  </form>
+                  <List>
+                    {form.prices.map(
+                      (value: number): JSX.Element => {
+                        const labelId = `checkbox-list-label-${value}`;
+                        return (
+                          <ListItem key={value} role={undefined} dense button>
+                            <ListItemText
+                              id={labelId}
+                              primary={`Precio: ${value}`}
+                            />
+                            <ListItemSecondaryAction>
+                              <IconButton
+                                edge="end"
+                                aria-label="comments"
+                                onClick={() => {
+                                  const newPrices = form.prices.filter(
+                                    (elem) => elem !== value
+                                  );
+
+                                  setForm({ ...form, prices: newPrices });
+                                }}
+                              >
+                                <DeleteIcon />
+                              </IconButton>
+                            </ListItemSecondaryAction>
+                          </ListItem>
+                        );
+                      }
+                    )}
+                  </List>
+                </Grid>
+              </Grid>
+            </Box>
+            <div className={classes.buttons}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={onSubmit}
+                className={classes.button}
+              >
+                Crear
+              </Button>
+            </div>
+          </React.Fragment>
+        )}
       </Paper>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Alerta</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Se agregó correctamente el producto al catálogo de productos o
+            servicios.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cerrar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
