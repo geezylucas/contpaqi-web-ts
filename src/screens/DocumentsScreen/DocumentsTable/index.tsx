23c7311be661f994151554523398e0d14f67f7ee
unknown <mike__221@hotmail.com>
2020-11-27 19:40:06 -0600

    Documents Screen and PDFDialog added successfully

--- src/screens/DocumentsScreen/DocumentsTable/index.tsx
+++ src/screens/DocumentsScreen/DocumentsTable/index.tsx
@@ -1,65 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import axios, { AxiosResponse } from "axios";
 import MaterialTable, { Column } from "material-table";
 import { TableIcons } from "../../../components";
+import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
+import PDFDialog from "../PDFDialog";
 
 type RowData = {
-  id: number;
-  email: string;
-  first_name: string;
-  last_name: string;
-  avatar: string;
+  codConcepto: number;
+  nombreConcepto: string;
+  folio: number;
+  serie: string;
+  fecha: string;
+  razonSocialCliente: string;
+  total: number;
+  pendiente: number;
 };
+
+type PdfType = {
+  open: boolean;
+  id: number;
+};
+
+const columns: Column<RowData>[] = [
+  { title: "Nombre Concepto", field: "nombreConcepto", type: "string" },
+  { title: "Concepto", field: "codConcepto", type: "numeric" },
+  { title: "Folio", field: "folio", type: "numeric" },
+  { title: "Fecha", field: "fecha", type: "datetime" },
+  { title: "Razón social", field: "razonSocialCliente", type: "string" },
+  { title: "Total", field: "total", type: "currency" },
+  { title: "Pendiente", field: "pendiente", type: "currency" },
+];
 
 interface IResponse {
   page: number;
-  per_page: number;
   total: number;
-  total_pages: number;
   data: RowData[];
 }
 
-const columns: Column<RowData>[] = [
-  {
-    title: "Avatar",
-    field: "avatar",
-    type: "string",
-    render: (rowData: RowData) => (
-      <img
-        style={{ height: 36, borderRadius: "50%" }}
-        src={rowData.avatar}
-        alt={`${rowData.first_name} ${rowData.last_name}`}
-      />
-    ),
-  },
-  { title: "First Name", field: "first_name", type: "string" },
-  { title: "Last Name", field: "last_name", type: "string" },
-];
-
 const DocumentsTable: React.FC<{}> = (): React.ReactElement => {
+  const [openPDF, setOpenPDF] = useState<PdfType>({ open: false, id: 0 });
+
   return (
-    <MaterialTable
-      title="Lista de documentos"
-      icons={TableIcons}
-      columns={columns}
-      data={(query) =>
-        new Promise((resolve, reject) => {
-          let url = "https://reqres.in/api/users?";
-          url += "per_page=" + query.pageSize;
-          url += "&page=" + (query.page + 1);
-          axios
-            .get<IResponse>(url)
-            .then((response: AxiosResponse<IResponse>) => {
-              resolve({
-                data: response.data.data,
-                page: response.data.page - 1,
-                totalCount: response.data.total,
+    <React.Fragment>
+      <MaterialTable
+        title="Lista de documentos"
+        icons={TableIcons}
+        columns={columns}
+        data={(query) =>
+          new Promise((resolve, reject) => {
+            let url = "http://localhost:5007/api/Documento/GetDocumentos?";
+            url += "PageNumber=" + (query.page + 1);
+            url += "&Rows=" + query.pageSize;
+            axios
+              .get<IResponse>(url)
+              .then((response: AxiosResponse<IResponse>) => {
+                resolve({
+                  data: response.data.data,
+                  page: response.data.page - 1,
+                  totalCount: response.data.total,
+                });
               });
-            });
-        })
-      }
-      options={{ showFirstLastPageButtons: false }}
-    />
+          })
+        }
+        actions={[
+          {
+            icon: () => <PictureAsPdfIcon />,
+            tooltip: "Save User",
+            onClick: (event, rowData: RowData | RowData[]): void =>
+              setOpenPDF({
+                id: Array.isArray(rowData) ? rowData[0].folio : rowData.folio,
+                open: true,
+              }),
+          },
+        ]}
+        options={{ showFirstLastPageButtons: false, actionsColumnIndex: -1 }}
+      />
+      <PDFDialog
+        open={openPDF.open}
+        handleClose={() => setOpenPDF({ ...openPDF, open: false })}
+        id={openPDF.id}
+      />
+    </React.Fragment>
   );
 };
 
---
+++ src/screens/DocumentsScreen/PDFDialog/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useTheme } from "@material-ui/core/styles";
+import { Document, Page, pdfjs } from "react-pdf";
+import { SizeMe } from "react-sizeme";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+type Props = {
+  open: boolean;
+  handleClose: () => void;
+  id: number;
+};
+
+const PDFDialog: React.FC<Props> = (props: Props): React.ReactElement => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const { open, handleClose, id } = props;
+  const pdf = `./docs/${id}.pdf`;
+
+  return (
+    <Dialog
+      fullScreen={fullScreen}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+      fullWidth={true}
+      maxWidth="md"
+    >
+      <DialogTitle id="form-dialog-title">PDF de documento {id}</DialogTitle>
+      <DialogContent>
+        <SizeMe>
+          {({ size }) => (
+            <Document file={pdf}>
+              <Page pageNumber={1} width={size.width ? size.width : 1} />
+            </Document>
+          )}
+        </SizeMe>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary" autoFocus>
+          Cerrar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default PDFDialog;
